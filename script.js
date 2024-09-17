3337d1d8bb58e2a808d31d28daf1c24ed5bf8272
Ziad Jweiles <151802002+clammytoast@users.noreply.github.com>
2024-09-17 17:19:18 +0000

    dark mode toggle theme update

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let isDarkMode = true;
 darkModeToggle.addEventListener('click', () => {
     if (isDarkMode) {
         document.body.classList.add('dark-mode');
-        darkModeToggle.textContent = 'Switch to Light Mode';
+        darkModeToggle.textContent = 'Switch to Dark Mode';
     } else {
         document.body.classList.remove('dark-mode');
-        darkModeToggle.textContent = 'Switch to Dark Mode';
+        darkModeToggle.textContent = 'Switch to Light Mode';
     }
     isDarkMode = !isDarkMode;
 });
