7c4cd0e811bda77e33820cad0d110cea22ff9bb8
Ziad Jweiles <151802002+clammytoast@users.noreply.github.com>
2024-09-17 16:18:09 +0000

    scripts.js fix?

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const body = document.body;
 
 checkbox.addEventListener('change', function () {
     if (this.checked) {
-        body.classList.toggle('dark-mode');
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
     }
 });
