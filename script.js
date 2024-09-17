ec04649f8dc9218e58898b255fb06f671522b592
Ziad Jweiles <151802002+clammytoast@users.noreply.github.com>
2024-09-17 17:11:01 +0000

    dark mode fix

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const checkbox = document.getElementById('dark-mode-toggle');
-const body = document.body;
+// Dark Mode Toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+let isDarkMode = true;
 
-checkbox.addEventListener('change', function () {
-    if (this.checked) {
-        body.classList.add('dark-mode');
+darkModeToggle.addEventListener('click', () => {
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+        darkModeToggle.textContent = 'Switch to Light Mode';
     } else {
-        body.classList.remove('dark-mode');
+        document.body.classList.remove('dark-mode');
+        darkModeToggle.textContent = 'Switch to Dark Mode';
     }
+    isDarkMode = !isDarkMode;
 });
